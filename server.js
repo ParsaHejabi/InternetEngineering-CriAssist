384a440eb3565b34a297c1e3324cb61a99e624d5
Parsa Hejabi <parsa.hejabi@gmail.com>
2020-09-16 19:42:24 +0430

    Updated whole project.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
